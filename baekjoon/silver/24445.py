401143362cf771250ea0a8ba82a0ad124d637c24
soleil094 <skqhddl7@gmail.com>
2022-10-28 21:02:33 +0900

    Add baekjoon

---
+++ baekjoon/silver/24445.py
@@ -0,0 +1,27 @@
+import sys
+input=sys.stdin.readline
+from collections import deque
+
+n, m, r = map(int, input().split())
+graph=[[] for _ in range(n+1)]
+visited=[0 for _ in range(n+1)]
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+q = deque([r])
+visited[r]=tmp=1
+
+while(q):
+    cur=q.popleft()
+    graph[cur].sort(reverse=True)
+    for i in graph[cur]:
+        if visited[i]==0:
+            tmp+=1
+            visited[i]=tmp
+            q.append(i)
+
+for i in visited[1::]:
+    print(i)
