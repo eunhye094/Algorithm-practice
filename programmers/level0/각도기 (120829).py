34fecef3c133349e02479fb207678aba8d59405e
soleil094 <skqhddl7@gmail.com>
2022-11-14 23:22:16 +0900

    Add Programmers Day 7

---
+++ programmers/level0/각도기 (120829).py
@@ -0,0 +1,10 @@
+def solution(angle):
+    if 0<angle<90:
+        return 1
+    elif angle==90:
+        return 2
+    elif 90<angle<180:
+        return 3
+    elif angle==180:
+        return 4
+    # return (angle//90)*2 + (angle%90!=0)
